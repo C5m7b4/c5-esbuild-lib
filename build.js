37f07e853d4fa667c84a73fc1e990feff5d7eba8
C5m7b4 <mbedingfield@dcrpos.com>
2023-04-03 16:49:43 -0500

    fixes testing

--- build.js
+++ build.js
@@ -1,34 +1,35 @@
-const { build } = require('esbuild');
-const { peerDependencies } = require('./package.json');
-const { Generator } = require('npm-dts');
+/* eslint-disable no-undef */
+const { build } = require("esbuild");
+const { devDependencies } = require("./package.json");
+const { Generator } = require("npm-dts");
 
 const shared = {
-  entryPoints: ['./src/index.ts'],
+  entryPoints: ["./src/index.ts"],
   bundle: true,
   minify: false,
   sourcemap: true,
-  external: Object.keys(peerDependencies),
+  external: Object.keys(devDependencies),
 };
 
 new Generator({
-  entry: './src/index.ts',
-  output: './dist/index.d.ts',
+  entry: "./src/index.ts",
+  output: "./dist/index.d.ts",
 }).generate();
 
-build({
-  ...shared,
-  outfile: './dist/index.js',
-});
+// build({
+//   ...shared,
+//   outfile: "./dist/index.js",
+// });
 
 build({
   ...shared,
-  outfile: './dist/esm/index.esm.js',
-  format: 'esm',
+  outfile: "./dist/esm/index.esm.js",
+  format: "esm",
 });
 
-build({
-  ...shared,
-  outfile: './dist/cjs/index.cjs.js',
-  platform: 'node',
-  target: ['node10.4'],
-});
+// build({
+//   ...shared,
+//   outfile: "./dist/cjs/index.cjs.js",
+//   platform: "node",
+//   target: ["node10.4"],
+// });
